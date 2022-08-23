4ddbb41fb40841b3722e564f84259c89d1673a70
SAMUEL <eng.njaumuchiri@gmail.com>
2022-08-23 16:21:46 +0300

    tkinter included

--- pdf translator.py
+++ pdf translator.py
@@ -1,9 +1,3 @@
 from pdfrw import PdfReader
-x = PdfReader('source.pdf')
-x.keys()
-['/Info', '/Size', '/Root']
-x.Info
-{'/Producer': '(cairo 1.8.6 (http://cairographics.org))',
- '/Creator': '(cairo 1.8.6 (http://cairographics.org))'}
-x.Root.keys()
-['/Type', '/Pages']
+x = PdfReader('example.pdf')
+print(x.info)
